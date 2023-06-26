f701324baf789a8aa6014f1084936a52fdc61305
shrikantkalar023 <shrikantkalar023@gmail.com>
2023-06-26 06:14:54 +0530

    Did some responsive optimizations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,30 +8,42 @@ export default function Home() {
   return (
     <main>
       <Navbar />
-      <h1 className="mx-4 mb-8 mt-32 text-center text-4xl font-extrabold leading-tight">
+      <h1 className="mx-4 mb-8 mt-32 text-center text-4xl font-extrabold leading-tight sm:mt-40 sm:font-black">
         Best way to grow your local business
       </h1>
-      <p className="mb-12 px-4 text-center text-sm font-light leading-6 tracking-wide">
+      <p className="mb-12 px-4 text-center text-sm font-light leading-6 tracking-wide sm:text-base">
         Artificial Intelligence powered easy-to-use tools to get more customers
         & give them a better experience
       </p>
-      <form className="mb-16 px-8">
+      <form className="mb-16 px-8 sm:mb-12 sm:flex sm:justify-center sm:gap-4">
         <input
           type="email"
           id="email"
-          className="mb-6 block w-full rounded-lg border border-slate-300  p-2.5 text-center text-sm text-gray-900 placeholder-slate-400 focus:border-sky-500 focus:outline-none focus:ring-0 focus:ring-sky-500"
+          className="mb-6 block w-full rounded-lg border border-slate-300 p-2.5 text-center text-sm text-gray-900 placeholder-slate-400 focus:border-sky-500 focus:outline-none focus:ring-0 focus:ring-sky-500 sm:mb-0 sm:max-w-2xl sm:basis-3/5"
           placeholder="ENTER YOUR EMAIL"
           required
         />
-        <div className="flex w-full justify-center">
+        <div className="mx-1 flex justify-center">
           <button
             type="submit"
-            className="rounded-lg bg-blue-600 px-7 py-3 text-center text-sm font-bold text-white hover:bg-blue-700"
+            className="whitespace-nowrap rounded-lg bg-blue-600  px-7 py-3 text-center text-sm font-bold text-white hover:bg-blue-700"
           >
             GET DEMO
           </button>
         </div>
       </form>
+      {/* TODO: remove this flex learn demo
+      <div className="flex justify-center gap-4 px-8">
+        <div className="max-w-2xl basis-2/3 bg-pink-500">01</div>
+        <div className="mx-1 flex justify-center">
+          <button
+            type="submit"
+            className="whitespace-nowrap rounded-lg bg-blue-600  px-7 py-3 text-center text-sm font-bold text-white hover:bg-blue-700"
+          >
+            GET DEMO
+          </button>
+        </div>
+      </div> */}
       <img src="/happy.webp" alt="happy local business person" />
       <h1 className="mx-4 mb-11 mt-20 text-center text-2xl font-black leading-snug tracking-wide">
         Trusted by 100,000+ local businesses globally
